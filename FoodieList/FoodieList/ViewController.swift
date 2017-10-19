0d3094b44a3243e5f85158a3493b7b4984539a55
Xiaojun Li <xiaojun@linktimecorp.com>
2017-10-19 11:05:15 -0700

    Added initial Xcode project files.

---
+++ FoodieList/FoodieList/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FoodieList
+//
+//  Created by Xiaojun Li on 10/19/17.
+//  Copyright © 2017 901CardinalDrive. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
